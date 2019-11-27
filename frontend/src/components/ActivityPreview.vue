ac82866eb36411b383fcb954b45e054036adbf89
ofer baram <my.phone.il@gmail.com>
2019-11-27 16:59:37 +0200

    delete preview bruch ofer

--- frontend/src/components/ActivityPreview.vue
+++ frontend/src/components/ActivityPreview.vue
@@ -1,5 +1,34 @@
 <template>
   <section class="preview-container">
-      <img src="" alt="">!!
+      <img src="../img/soccer.jpg">
+
   </section>
 </template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  // prop:['activity'],
+  
+  // methods:{
+  //    open() {
+  //       this.$message.success('This is a message.');
+  //     },
+  //   saveToy(){
+  //     this.$store.dispatch({type:'editToy', toy:this.toy})
+  //     .then(()=>this.$router.push('/'))
+  //   }
+  // },
+  // created(){
+  //   const toyId = this.$route.params.id;
+  //   if(!toyId) return this.$router.push('/')
+  //   this.$store.dispatch({type:'setCurrToy', toy:toyId})
+  //     .then(()=> {
+  //       this.toy = JSON.parse(JSON.stringify(this.$store.getters.getCurrToy))
+  //     })
+  // }
+};
+</script>
