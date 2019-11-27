ff05e2a1778174452efbfc8049866e64b5ae0d41
Daniel Yona <danielyona93@gmail.com>
2019-11-27 18:06:19 +0200

    activity store

---
+++ frontend/src/services/ActivityService.js
@@ -0,0 +1,30 @@
+import { httpService } from './HttpService.js';
+
+const BASE_URL = 'http://localhost:3000/'
+
+function getActivities(){
+    return httpService.get(BASE_URL + '/activities').then(res => res.data)
+}
+
+function getActivity(id){
+    return httpService.get(BASE_URL + `/activity${id}`).then(res => res.data)
+}
+
+function addActivity(activity){
+    if(!activity.id){ // Needs to be removed when backend is alive
+        activity.id = Math.floor(Math.random() * 1000 + 5000);
+        return httpService.post(BASE_URL + '/activity', activity).then(res => res.data);
+    }
+    return httpService.put(BASE_URL + `/activity${activity.id}`, activity).then(res => res.data);
+}
+
+function removeActivity(id){
+    return httpService.delete(BASE_URL + `/activity/${id}`)
+}
+
+export default {
+    getActivities,
+    getActivity,
+    addActivity,
+    removeActivity
+}
--- frontend/src/store/activity.store.js
+++ frontend/src/store/activity.store.js
@@ -6,11 +6,34 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true,
   state: {
-    activities:[],
+    activities: {
+      all: [],
+      popular: [],
+      recent: [],
+      today: [],
+    },
   },
   mutations: {
+    setActivities(state, type, activities) {
+      state.activities[type] = activities;
+    },
   },
   actions: {
+    
+  },
+  getters: {
+    all(){
+      return this.state.all;
+    },
+    popular(){
+      return this.state.popular;
+    },
+    recent(){
+      return this.state.recent;
+    },
+    today(){
+      return this.state.today;
+    },
   },
   modules: {
   }
