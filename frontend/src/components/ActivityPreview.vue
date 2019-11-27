9a3d9270d1bc3496141fe805a37cdc26bb31a0a2
ofer baram <my.phone.il@gmail.com>
2019-11-27 17:33:43 +0200

    preview changes

--- frontend/src/components/ActivityPreview.vue
+++ frontend/src/components/ActivityPreview.vue
@@ -1,7 +1,18 @@
 <template>
   <section class="preview-container">
       <img src="../img/soccer.jpg">
-
+      <div class="preview-details">
+            <div class="preview-info">
+              {{activity.title}}
+            </div>
+          <div class="preview-desc">
+              {{activity.description}}
+          </div>
+          <div class="preview-creator">
+              {{activity.imgUrl}}
+              {{activity.fullName}}
+          </div>
+      </div>
   </section>
 </template>
 
@@ -11,24 +22,6 @@
 
 <script>
 export default {
-  // prop:['activity'],
-  
-  // methods:{
-  //    open() {
-  //       this.$message.success('This is a message.');
-  //     },
-  //   saveToy(){
-  //     this.$store.dispatch({type:'editToy', toy:this.toy})
-  //     .then(()=>this.$router.push('/'))
-  //   }
-  // },
-  // created(){
-  //   const toyId = this.$route.params.id;
-  //   if(!toyId) return this.$router.push('/')
-  //   this.$store.dispatch({type:'setCurrToy', toy:toyId})
-  //     .then(()=> {
-  //       this.toy = JSON.parse(JSON.stringify(this.$store.getters.getCurrToy))
-  //     })
-  // }
+  prop:['activity']
 };
 </script>
