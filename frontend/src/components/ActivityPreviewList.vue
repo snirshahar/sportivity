c1af87ae686c1ddfee957324244272ff54016c77
Snir Shahar <shahar.snir91@gmail.com>
2019-11-27 16:31:37 +0200

    Testing first branch

---
+++ frontend/src/components/ActivityPreviewList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="activity-list">
+    <h1>Most Popular Activities At The Moment</h1>
+    <div v-for="activity in activities" :key="activity._id"></div>
+  </section>
+</template>
+
+<script>
+// import activityPreview from './activity-preview.vue'
+
+export default {
+  props: ["activities"],
+  data() {
+    return {};
+  },
+  methods: {
+    // deleteActivity(activityId) {
+    //   this.$emit("deleteActivity", activityId);
+    // }
+  },
+  components: {
+    //   TEST TEST TEST!
+    // activityPreview
+  }
+};
+</script>
+
+<style scoped>
+.activity-list {
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 15px;
+}
+</style>
