76589016581adf9dedc7db9ecc7a5b58b9a6d232
ofer baram <my.phone.il@gmail.com>
2019-11-28 15:06:00 +0200

    login change

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
-    <h1>user Login signup here</h1>
+    <form @submit.prevent="userLoggin">
+      <p>Email</p>
+      <input type="text" v-model="user.email">
+      <p>Password</p>
+      <input type="password" v-model="user.password">
+      <button>Loggin</button>
+    </form>
   </div>
 </template>
+
+<script>
+import ActivityPreview from "../components/ActivityPreview";
+
+export default {
+  data() {
+    return {
+      user:{
+        email:'',
+        password:''
+      }
+    };
+  },
+  methods:{
+    userLoggin(){
+      console.log(this.user);
+      
+    }
+  },
+  created() {
+    
+    // this.$store.dispatch("loadActivities");
+  },
+  components: {
+  }
+};
+</script>
