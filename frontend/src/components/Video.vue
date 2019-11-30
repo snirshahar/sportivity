418334334be28bb9c82ad6f32a85d8e59b6585c6
daniely-93 <danielyona93@gmail.com>
2019-11-30 15:29:08 +0200

    Added geolocation + design changes

---
+++ frontend/src/components/Video.vue
@@ -0,0 +1,22 @@
+<template>
+  <vue-video-section
+    :elementId="'header-background-video'"
+    :ref="'header-background-video'"
+    :mp4Source="require('../assets/videos/home.mp4')"
+    :posterSource="require('../assets/logo.png')"
+    :mobileBreakpoint="992"
+    :desktopHeight="550"
+    :mobileHeight="450"
+    :playsinline="true"
+    :loop="true"
+    :autoplay="true"
+    :autobuffer="true"
+    :muted="true"
+  ></vue-video-section>
+</template>
+
+<style>
+.vue-video-section-wrapper {
+  z-index: -1;
+}
+</style>
