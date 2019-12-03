2f3f2bda02c65e4e8e61b3aa0e28f1b833eaf91b
Snir Shahar <shahar.snir91@gmail.com>
2019-12-03 12:27:23 +0200

    filter cmp

--- frontend/src/components/ActivityFilter.vue
+++ frontend/src/components/ActivityFilter.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    activity filter here ?????????
-  </div>
+  <section>
+    <div class="explore-categories">
+      <button @click="filterBy='all'">All</button>
+      <button @click="filterBy='soccer'">Soccer</button>
+      <button @click="filterBy='basketball'">Basketball</button>
+      <button @click="filterBy='tennis'">Tennis</button>
+      <button @click="filterBy='volleyball'">Volleyball</button>
+      <button @click="filterBy='workout'">Workout</button>
+      <button @click="filterBy='pool'">Pool</button>
+      <button @click="filterBy='poker'">Poker</button>
+      <button @click="filterBy='yoga'">Yoga</button>
+      <button @click="filterBy='cycling'">Cycling</button>
+      <button @click="filterBy='dance'">Dance</button>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  name: "ActivityFilter",
+  data() {
+    return {
+      filterBy: "all",
+      activities: null
+    };
+  },
+  computed: {
+    filterActivities() {
+      const activities = this.$store.getters.activities;
+      return this.filterBy === "all"
+        ? activities
+        : activities.filter(activity => activity.category === this.filterBy);
+    }
+  },
+  watch: {
+    filterBy() {
+      this.activities = this.filterActivities;
+      console.log("filtered:", this.filterActivities);
+    }
+  }
+};
+</script>
