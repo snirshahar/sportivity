fb50116b25e0626019a13590b5330f9e55e949a2
Daniel Yona <danielyona93@gmail.com>
2019-12-05 12:39:31 +0200

    fixed heroku bugs

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://daniel1:daniel1@cluster0-bdymc.mongodb.net/test?retryWrites=true&w=majority",
+  "dbURL": `mongodb+srv://daniel1:${encodeURIComponent(daniel1)}@cluster0-bdymc.mongodb.net/test?retryWrites=true&w=majority`,
 }
