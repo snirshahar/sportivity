e8289a6531debe18a6bfb54fee0b94d45fa28a8f
Daniel Yona <danielyona93@gmail.com>
2019-12-05 18:41:31 +0200

    fixed socket

--- frontend/src/services/SocketService.js
+++ frontend/src/services/SocketService.js
@@ -1,9 +1,10 @@
 import io from 'socket.io-client';
 
 const BASE_URL = process.env.NODE_ENV !== 'development'
-    ? '/api/'
+    ? '/'
     : '//localhost:3000/'
 const socket = io(BASE_URL)
+console.log(socket)
 
 export default {
     on,
@@ -11,10 +12,12 @@ export default {
 }
 
 function on(eventName, cb) {
+    console.log(`SocketService, emit ${eventName}, ${data}`)
     socket.on(eventName, cb)
 }
 
 function emit(eventName, data) {
+    console.log(`SocketService, emit ${eventName}, ${data}`)
     socket.emit(eventName, data)
 }
 
