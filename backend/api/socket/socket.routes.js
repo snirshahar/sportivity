27435d8157ae5b81bd514fe46eaef547661b0b53
Daniel Yona <danielyona93@gmail.com>
2019-12-05 18:43:07 +0200

    fixed socket

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -3,24 +3,28 @@ const activityService = require('../activity/activity.service')
 module.exports = connectSockets
 
 function connectSockets(io) {
-io.on('connection', socket => {
-    socket.on('user listen', ({activityId}) =>{
-        console.log(activityId);
-        socket.join(activityId)
-        socket.activityId = activityId;
+    io.on('connection', socket => {
+        socket.on('user listen', ({ activityId }) => {
+            console.log('user listen');
+            socket.join(activityId)
+            socket.activityId = activityId;
+        })
+        socket.on('user joined', ({ activityId, user }) => {
+            console.log('user join');
+
+            socket.activityId = activityId;
+            io.to(socket.activityId).emit('msg recieved', { from: `${user.fullName} joined the activity` })
+        })
+        socket.on('user left', ({ activityId, user }) => {
+            console.log('user left')
+            socket.activityId = activityId;
+            io.to(socket.activityId).emit('msg recieved', { from: `${user.fullName} left the activity` })
+            socket.leave(activityId)
+        })
+        socket.on('chat addMsg', async (msg) => {
+            console.log('chat addMsg')
+            const newMsg = await activityService.addMsg(msg.activityId, msg)
+            io.to(msg.activityId).emit('msg recieved', newMsg)
+        })
     })
-    socket.on('user joined', ({activityId, user}) =>{
-        socket.activityId = activityId;
-        io.to(socket.activityId).emit('msg recieved', {from:`${user.fullName} joined the activity`})
-    })
-    socket.on('user left', ({activityId, user}) =>{
-        socket.activityId = activityId;
-        io.to(socket.activityId).emit('msg recieved', {from:`${user.fullName} left the activity`})
-        socket.leave(activityId)
-    })
-    socket.on('chat addMsg', async (msg) =>{
-        const newMsg = await activityService.addMsg(msg.activityId ,msg)
-        io.to(msg.activityId).emit('msg recieved', newMsg)
-    })
-})
 }
