c3b7e8e9c943ac1f96f062de749812d3b9b11a6c
ofer baram <my.phone.il@gmail.com>
2019-12-07 17:40:31 +0200

    ofer changes soket before shabat

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,15 +8,34 @@
 
 <script>
 import NavBar from "./components/NavBar";
+import SocketService from "./services/SocketService";
 import Footer from "./components/Footer";
+import ActivityService from "./services/ActivityService";
+
 
 export default {
   components: {
     Footer,
     NavBar
   },
-  created() {
+  async created() {
     this.$store.dispatch("loadActivities");
-  }
-};
+    const activities = await ActivityService.getActivities();
+    const user = this.$store.getters.loggedinUser;
+    SocketService.emit("single socket", { user })
+        console.log('user', user);
+    console.log('activities', activities);
+    if(user && activities){
+      activities.forEach(activity=> {
+        activity.attendees.forEach(attendee=>{
+          if(activity._id===user._id){
+            console.log('user');
+            
+            SocketService.emit("user connect", { activityId: activity._id })
+        }
+        })
+      })
+    } else  SocketService.emit("single socket", { user: 'guest' })
+  } 
+}
 </script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -117,6 +117,9 @@
 
 <script>
 import ActivityPreview from "../components/ActivityPreview";
+import ActivityService from "../services/ActivityService";
+import SocketService from "../services/SocketService";
+
 // import UserService from '../services/UserService';
 
 export default {
@@ -143,6 +146,24 @@ export default {
       }
       this.$router.push("/");
     }
-  }
+  },
+  async created(){
+    const activities = await ActivityService.getActivities();
+  //   const user = this.$store.getters.loggedinUser;
+  //   SocketService.emit("single socket", { user })
+  //       console.log('user', user);
+  //   console.log('activities', activities);
+  //   if(user && activities){
+  //     activities.forEach(activity=> {
+  //       activity.attendees.forEach(attendee=>{
+  //         if(activity._id===user._id){
+  //           console.log('user');
+            
+  //           SocketService.emit("user connect", { activityId: activity._id })
+  //       }
+  //       })
+  //     })
+  //   } else  SocketService.emit("single socket", { user: 'guest' })
+  } 
 };
 </script>
