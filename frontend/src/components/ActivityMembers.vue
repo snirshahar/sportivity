c981c5f760ebf71a0c4cd1e197e7170ba16964bb
ofer baram <my.phone.il@gmail.com>
2019-12-08 11:29:34 +0200

    ofer socket

--- frontend/src/components/ActivityMembers.vue
+++ frontend/src/components/ActivityMembers.vue
@@ -2,7 +2,7 @@
   <div class="activity-details-section attendees-container" v-if="attendees">
     <h1>Members</h1>
     <div class="attendees">
-      <ActivityPreview v-for="attendee in attendees" :key="attendee._uid" :attendee="attendee" :size="'50px'" :showName="true"></ActivityPreview>
+      <ActivityPreview v-for="attendee in attendees" :key="attendee._uid" :attendee="attendee" size="'50px'" :showName="true"></ActivityPreview>
     </div>
   </div>
 </template>
