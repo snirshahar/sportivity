3ae89b32c7b41476bfd17f8ee97d4a9a5be8c56c
Daniel Yona <danielyona93@gmail.com>
2019-12-08 14:18:41 +0200

    Sockets

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -1,53 +1,52 @@
 const activityService = require('../activity/activity.service')
-
 module.exports = connectSockets
-
 function connectSockets(io) {
     io.on('connection', socket => {
-        socket.on('user listen', ({ activityId }) => {
-            console.log('user listen');
-            socket.on('single socket', ({ user }) => {
-                if (!user) return;
-                else if (user === 'guest') {
-                    const num = activityService.randomId()
-                    user = { _id: num, fullName: `guest${num}` }
-                }
-                const userId = user._id
-                socket.join(userId)
-                io.to(userId).emit('msg to single user', `${user.fullName} has connected`);
+        socket.on('single socket', ({ user }) => {
+            if(!user) return;
+            if(user ==='guest'){
+                const num = Math.floor(Math.random() * 1000000);
+                user = {_id: num, fullName: `guest${num}`}
+            } 
+            const userId = user._id
+            socket.join(userId);
+            io.to(userId).emit('msg to single user', `${user.fullName} has connected`);
+        })
+        socket.on('user connect to socket activity', ({ activityId }) => {
+            socket.activityId = activityId;
+            socket.join(activityId)
+        })
+        socket.on('user listen activity', ({ activityId }) => {
+            socket.activityId = activityId;
+            socket.join(activityId)
+        })
+        socket.on('user unListen activity', ({ activityId }) => {
+            delete socket.activityId;
+            socket.leave(activityId)
+        })
+        socket.on('user joined', async ({ activityId, user }) => {
+            const activity = await activityService.getActivity(activityId);
+            socket.join(user._id)
+            socket.join(activityId)
+            socket.activityId = activityId;
+            socket.to(socket.activityId).emit('msg to all activity members except sender', `${user.fullName} has joined ${activity.title}`);
+            io.to(user._id).emit('msg to single user', `You joined ${activity.title}`);
+            activity.attendees.map(attendee => {
+                console.log(attendee)
+                // attendee.inbox.unshift({from: 'System', msg: `${user.fullName} has joined ${activity.title}` })
             })
-            socket.on('user listen activity', ({ activityId }) => {
-                socket.activityId = activityId;
-                socket.join(activityId)
-            })
-            socket.on('user unListen activity', ({ activityId }) => {
-                delete socket.activityId;
-                socket.leave(activityId)
-            })
-            socket.on('user connect to socket activity', ({ activityId }) => {
-                socket.activityId = activityId;
-                socket.join(activityId)
-                socket.activityId = activityId;
-            })
-            socket.on('user joined', ({ activityId, user }) => {
-                socket.join(user._id)
-                socket.join(activityId)
-                socket.activityId = activityId;
-                socket.to(socket.activityId).emit('msg to all activity members except sender', `${user.fullName} has joined the activity`);
-                io.to(user._id).emit('msg to single user', `${user.fullName} you just connected`);
-            })
-            socket.on('user unjoined', ({ activityId, user }) => {
-                socket.activityId = activityId;
-                socket.to(socket.activityId).emit('msg to all activity members except sender', `${user.fullName} has left the activity`);
-                io.to(user._id).emit('msg to single user', `${user.fullName} you just unconnected`);
-                socket.leave(activityId)
-                socket.leave(user._id)
-            })
-            socket.on('chat addMsg', async (msg) => {
-                console.log('chat addMsg')
-                const newMsg = await activityService.addMsg(msg.activityId, msg)
-                io.to(msg.activityId).emit('msg recieved', newMsg)
-            })
+        })
+        socket.on('user unjoined', async ({ activityId, user }) => {
+            const activity = await activityService.getActivity(activityId);
+            socket.activityId = activityId;
+            socket.to(socket.activityId).emit('msg to all activity members except sender', `${user.fullName} has left ${activity.title}`);
+            io.to(user._id).emit('msg to single user', `You left ${activity.title}`);
+            socket.leave(activityId)
+            socket.leave(user._id)
+        })
+        socket.on('chat addMsg', async (msg) => {
+            const newMsg = await activityService.addMsg(msg.activityId, msg)
+            io.to(msg.activityId).emit('msg recieved', newMsg)
         })
     })
 }
